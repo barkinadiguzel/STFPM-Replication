701550c6401f6fb8d867e37e0070d290f16775e6
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-03-20 15:04:40 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,10 @@
+BACKBONE = "resnet18"           
+PRETRAINED = True                
+FREEZE_TEACHER = True            
+
+FEATURE_LAYERS = ["layer1", "layer2", "layer3"] 
+
+ALPHA = [1.0, 1.0, 1.0]          
+
+UPSAMPLE_MODE = "bilinear"         
+ALIGN_CORNERS = False             
